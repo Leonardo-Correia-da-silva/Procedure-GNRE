319105a48c8c9315b80db56cf20221d3da812f3c
Vanessa <vanessa@cnesistemas.com.br>
2024-05-27 16:27:35 -0300

    procedure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo1 from '../src/Imagens GNRE/favicon.ico';
+import logo2 from '../src/Imagens GNRE/logo-GNRE.png';
+import Login from '../src/Imagens GNRE/Login.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='div-logo'>
+    
+        <header>
+          <img className='logo1' src={logo1} />
+          <img className='logo2' src={logo2} />
+          <h1 className='titulo'>Procedimento - Enviar GNRE</h1>
+        </header>
+      </div>
+
+      <div>
+        <main className='principal'>
+          <h3> ENTRAR NO SISTEMA E FAZER LOGIN</h3>
+          <p>&bull; Entre no sistema ERP Client, faça o login com seu usuário e senha,
+            clique em "ok" para ser direcionado ao ambiente do sistema! (conforme imagem a seguir):</p>
+          <img className='img-proced-1' src={Login} />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/gestão-vendas-NFe/gvn.js
@@ -0,0 +1,38 @@
+import "../gestão-vendas-NFe/gvn.css";
+import proced2 from '../Imagens GNRE/proced.2.png';
+import proced3 from '../Imagens GNRE/proced.3.png';
+import proced4 from '../Imagens GNRE/proced.4.png';
+import proced5 from '../Imagens GNRE/proced.5.png';
+import proced6 from '../Imagens GNRE/proced.6.png';
+
+function GVN() {
+    return (
+        <div className="GVN">
+            <h3>GESTÃO DE VENDAS / NOTA FISCAL ELETRÔNICA</h3>
+            <p>&bull; Entre na area de "Gestão de Vendas", em seguida na parte de "Nota Fiscal Eletrônica" (conforme imagem a seguir):</p>
+            <img className='img-proced-1' src={proced2} />
+
+
+            <h3>SELEÇÃO DA EMPRESA</h3>
+            <p>&bull; Selecione o item "empresa", escolha a empresa e aperte em "confirma", (conforme imagem a seguir):</p>
+            <img className='img-proced-1' src={proced3} />
+
+
+            <h3>BUSCAR POR FILTRO DE DATAS</h3>
+            <p>&bull; selecione o periodo das datas desejadas, e em seguida selecione o ícone(binóculo) ao lado, para pesquisar (conforme imagem a seguir):</p>
+            <img className='img-proced-1' src={proced4} />
+
+
+            <h3>FLEGAR OS ITENS REQUERIDOS</h3>
+            <p>&bull; Flegar os ícones que deseja enviar, (conforme imagem a seguir):</p>
+            <img className='img-proced-1' src={proced5} />
+
+
+            <h3>FINALIZANDO O ENVIO</h3>
+            <p>&bull; Por fim para finalizarmos, selecione o item "Gerar e Enviar" (conforme imagem a seguir):</p>
+            <img className='img-proced-1' src={proced6} />
+
+        </div>
+    )
+};
+export default GVN;
